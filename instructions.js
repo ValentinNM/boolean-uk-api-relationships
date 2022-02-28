fffc22934abea53ba364a4281a6d31fad30ed735
ValentinNM <valentinm.web@gmail.com>
2022-02-28 21:03:31 +0200

    last update before deleting the directory locally

---
+++ instructions.js
@@ -0,0 +1,20 @@
+// Today's exercise: Relationships with Prisma
+
+// Repo: boolean-uk-api-relationships
+
+// Use npx prisma studio to add some mock data to your models using "Add Record".
+
+// Build these routes and controllers for the users resource that work with the following fetch requests:
+//  - /users returns all users
+//  - /users/:id returns a specific user
+//  - /users/:id/address returns a specific user with an address
+//  - /users/:id/orders returns a specific user with a list of orders
+//  - /users/:id/orders?before=specific-time returns a specific user with a list of orders created before a specific time
+//  - /users?city=specific-city returns a list of users from a specific city
+
+// Tips
+// - Check the SQL after every request to see what's happening under the hood
+// - Use the prisma documentation
+
+// Challenge
+// Add a Product model with an inventory model.
